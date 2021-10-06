8fd816578e8f581f42c2717835d08345e392482d
Daniel-Alencar <danielalencar746@gmail.com>
2021-10-06 11:13:02 -0300

    cleaning

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -1,56 +1,18 @@
-let rects = [];
-let cols;
-let rows;
-let resolution = 10;
-
-class MyRect {
-  constructor() {
-    this.px = pwinMouseX;
-    this.py = pwinMouseY;
-    this.x = winMouseX;
-    this.y = winMouseY;
-  }
-  show() {
-    let PX = this.px - (this.px % resolution);
-    let PY = this.py - (this.py % resolution);
-    stroke(0);
-    fill(255);
-    rect(PX, PY, resolution, resolution);
-
-    let X = this.x - (this.x % resolution);
-    let Y = this.y - (this.y % resolution);
-    stroke(0);
-    fill(255);
-    rect(X, Y, resolution, resolution);
-  }
-}
+let lines = [];
 
 function setup() {
-  createCanvas(400, 500);
-  cols = width / resolution;
-  rows = height / resolution;
+  createCanvas(document.body.offsetWidth, document.body.offsetHeight);
 }
 
 function draw() {
-  background(0);
-
-  for(let i = 0; i < cols; i++) {
-    for(let j = 0; j < rows; j++) {
-      let x = i * resolution;
-      let y = j * resolution;
-
-      fill(0);
-      stroke(255);
-      rect(x, y, resolution - 1, resolution - 1);
-    }
-  }
+  background(30);
 
   if(mouseIsPressed) {
-    let rect = new MyRect();
-    rects.push(rect);
+    let line = new MyLine();
+    lines.push(line);
   }
 
-  for(let rect of rects) {
-    rect.show();
+  for(let line of lines) {
+    line.show();
   }
 }
